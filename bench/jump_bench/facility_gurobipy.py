2f8e7be74902fbfa374ba982aa15b4c1802427b3
metab0t <metab0t@outlook.com>
2024-02-14 23:59:34 +0800

    Update copyright

--- bench/jump_bench/facility_gurobipy.py
+++ bench/jump_bench/facility_gurobipy.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022: Miles Lubin and contributors
+# Copyright (c) 2023: Yue Yang
 #
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
