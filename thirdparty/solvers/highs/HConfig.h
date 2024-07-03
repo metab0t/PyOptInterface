bccd3cf3545da148a1b20c3e8b5c933fbf8373ff
metab0t <metab0t@outlook.com>
2024-07-03 22:59:37 +0800

    Update HiGHS headers to 1.7.2

--- thirdparty/solvers/highs/HConfig.h
+++ thirdparty/solvers/highs/HConfig.h
@@ -5,18 +5,17 @@
 /* #undef ZLIB_FOUND */
 /* #undef CUPDLP_CPU */
 #define CMAKE_BUILD_TYPE "Release"
-#define CMAKE_INSTALL_PREFIX "D:/a/highs_autobuild/highs_autobuild/install"
+#define CMAKE_INSTALL_PREFIX "D:/a/highsbox/highsbox/HiGHS/highs_dist"
 /* #undef HIGHSINT64 */
 /* #undef HIGHS_NO_DEFAULT_THREADS */
 #define HIGHS_HAVE_MM_PAUSE
 /* #undef HIGHS_HAVE_BUILTIN_CLZ */
 #define HIGHS_HAVE_BITSCAN_REVERSE
 
-#define HIGHS_GITHASH "50670fd"
-#define HIGHS_COMPILATION_DATE "2024-03-07"
+#define HIGHS_GITHASH "5ce7a27"
 #define HIGHS_VERSION_MAJOR 1
 #define HIGHS_VERSION_MINOR 7
-#define HIGHS_VERSION_PATCH 0
-#define HIGHS_DIR "D:/a/highs_autobuild/highs_autobuild"
+#define HIGHS_VERSION_PATCH 2
+#define HIGHS_DIR "D:/a/highsbox/highsbox/HiGHS"
 
 #endif /* HCONFIG_H_ */
