fd8952397a3ac5515c40591820a24bd3f6621060
metab0t <metab0t@outlook.com>
2024-10-20 17:24:51 +0800

    Update HiGHS header to 1.8.0

--- thirdparty/solvers/highs/HConfig.h
+++ thirdparty/solvers/highs/HConfig.h
@@ -1,21 +1,21 @@
 #ifndef HCONFIG_H_
 #define HCONFIG_H_
 
-#define FAST_BUILD
+/* #undef FAST_BUILD */
 /* #undef ZLIB_FOUND */
 /* #undef CUPDLP_CPU */
 #define CMAKE_BUILD_TYPE "Release"
-#define CMAKE_INSTALL_PREFIX "D:/a/highsbox/highsbox/HiGHS/highs_dist"
+#define CMAKE_INSTALL_PREFIX "E:/HiGHS/build/install"
 /* #undef HIGHSINT64 */
 /* #undef HIGHS_NO_DEFAULT_THREADS */
 #define HIGHS_HAVE_MM_PAUSE
 /* #undef HIGHS_HAVE_BUILTIN_CLZ */
 #define HIGHS_HAVE_BITSCAN_REVERSE
 
-#define HIGHS_GITHASH "5ce7a27"
+#define HIGHS_GITHASH "fcfb53414"
 #define HIGHS_VERSION_MAJOR 1
-#define HIGHS_VERSION_MINOR 7
-#define HIGHS_VERSION_PATCH 2
-#define HIGHS_DIR "D:/a/highsbox/highsbox/HiGHS"
+#define HIGHS_VERSION_MINOR 8
+#define HIGHS_VERSION_PATCH 0
+#define HIGHS_DIR "E:/HiGHS"
 
 #endif /* HCONFIG_H_ */
