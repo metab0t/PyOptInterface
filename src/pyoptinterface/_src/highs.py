fb7ef4590275733e6670f7fb8de9037d8bf8bedf
metab0t <metab0t@outlook.com>
2025-01-19 20:59:18 +0800

    Format fix

--- src/pyoptinterface/_src/highs.py
+++ src/pyoptinterface/_src/highs.py
@@ -222,7 +222,9 @@ model_attribute_get_func_map = {
     ModelAttribute.ObjectiveValue: lambda model: model.get_obj_value(),
     ModelAttribute.SolveTimeSec: lambda model: model.getruntime(),
     ModelAttribute.NumberOfThreads: lambda model: model.get_raw_option_int("threads"),
-    ModelAttribute.RelativeGap: lambda model: model.get_raw_option_double("mip_rel_gap"),
+    ModelAttribute.RelativeGap: lambda model: model.get_raw_option_double(
+        "mip_rel_gap"
+    ),
     ModelAttribute.TimeLimitSec: lambda model: model.get_raw_option_double(
         "time_limit"
     ),
