eb25c1edfe135c3614e347d62c79b571d894ca43
metab0t <metab0t@outlook.com>
2025-03-21 00:02:59 +0800

    Update HiGHS headers to 1.10

--- thirdparty/solvers/highs/HConfig.h
+++ thirdparty/solvers/highs/HConfig.h
@@ -3,7 +3,9 @@
 
 #define FAST_BUILD
 /* #undef ZLIB_FOUND */
-/* #undef CUPDLP_CPU */
+#define CUPDLP_CPU
+/* #undef CUPDLP_GPU */
+/* #undef CUPDLP_FORCE_NATIVE */
 #define CMAKE_BUILD_TYPE "Release"
 #define CMAKE_INSTALL_PREFIX "E:/HiGHS/build/install"
 /* #undef HIGHSINT64 */
@@ -12,9 +14,9 @@
 /* #undef HIGHS_HAVE_BUILTIN_CLZ */
 #define HIGHS_HAVE_BITSCAN_REVERSE
 
-#define HIGHS_GITHASH "66f735e60"
+#define HIGHS_GITHASH "fd8665394"
 #define HIGHS_VERSION_MAJOR 1
-#define HIGHS_VERSION_MINOR 9
+#define HIGHS_VERSION_MINOR 10
 #define HIGHS_VERSION_PATCH 0
 #define HIGHS_DIR "E:/HiGHS"
 
