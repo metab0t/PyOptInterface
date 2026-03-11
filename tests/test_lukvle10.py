ab8b6528cb19610963f1d0e9be6e0ef69a1020b6
eminyouskn <youssouf.emine@artelys.com>
2026-03-11 15:52:54 -0400

    Update LUKVLE10 test: correct starting points, increase variable count, and refine objective function

--- tests/test_lukvle10.py
+++ tests/test_lukvle10.py
@@ -10,7 +10,7 @@ def test_nlp_lukvle10(nlp_model_ctor):
     # min  sum[i=1..n] (x[2i]^2)^(x[2i+1]^2 + 1) + (x[2i+1]^2)^(x[2i]^2 + 1)
     # s.t. (3 - 2*x[i+1])*x[i+1] + 1 - x[i] - 2*x[i+2] = 0,  i = 1,...,2n-2
     #
-    # Starting point: x[2i] = 1, x[2i+1] = -1
+    # Starting point: x[2i] = -1, x[2i+1] = 1
     model = nlp_model_ctor()
     if isinstance(model, ipopt.Model):
         # LUKVLE10 is too large and IPOPT raises a bad_alloc error.
@@ -20,7 +20,7 @@ def test_nlp_lukvle10(nlp_model_ctor):
         # to 2000 variables.
         pytest.skip("lukvle10 is too large to be supported with COpt")
 
-    n = 100000
+    n = 125000
     x = model.add_m_variables(2 * n, name="x")
 
     for i in range(2 * n - 2):
@@ -33,22 +33,19 @@ def test_nlp_lukvle10(nlp_model_ctor):
 
     with nl.graph():
         for i in range(n):
-            x2i_sq = x[2 * i] * x[2 * i]
-            x2ip1_sq = x[2 * i + 1] * x[2 * i + 1]
             model.add_nl_objective(
-                nl.pow(x2i_sq, x2ip1_sq + 1) + nl.pow(x2ip1_sq, x2i_sq + 1)
+                nl.pow(x[2 * i] ** 2, (x[2 * i + 1] ** 2) + 1)
+                + nl.pow(x[2 * i + 1] ** 2, (x[2 * i] ** 2) + 1)
             )
 
     for i in range(n):
-        model.set_variable_attribute(x[2 * i], poi.VariableAttribute.PrimalStart, 1.0)
+        model.set_variable_attribute(x[2 * i], poi.VariableAttribute.PrimalStart, -1.0)
         model.set_variable_attribute(
-            x[2 * i + 1], poi.VariableAttribute.PrimalStart, -1.0
+            x[2 * i + 1], poi.VariableAttribute.PrimalStart, 1.0
         )
 
     model.optimize()
+    model.set_model_attribute(poi.ModelAttribute.Silent, False)
 
     termination_status = model.get_model_attribute(poi.ModelAttribute.TerminationStatus)
-    assert (
-        termination_status == poi.TerminationStatusCode.LOCALLY_SOLVED
-        or termination_status == poi.TerminationStatusCode.OPTIMAL
-    )
+    assert termination_status == poi.TerminationStatusCode.OPTIMAL
